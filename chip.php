9a200951e1df4216534da3f915cead24d1c318ed
crohmn <61759223+crohmn@users.noreply.github.com>
2020-03-06 16:30:26 +0700

    Update and rename go.php to chip.php

--- chip.php
+++ chip.php
@@ -6,11 +6,11 @@ $header[] = "User-Agent: okhttp/3.10.0";
 $header[] = "Accept: application/json";
 $header[] = "Accept-Language: en-ID";
 $header[] = "Content-Type: application/json; charset=UTF-8";
-$header[] = "X-AppVersion: 3.30.2";
+$header[] = "X-AppVersion: 3.48.2";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: en_ID";
-//$header[] = "X-Location: -6.3894201,106.0794195";
+//$header[] = "X-Location: -7.563477,110.814675";
 //$header[] = "X-Location-Accuracy: 3.0";
 if ($pin):
 $header[] = "pin: $pin";
